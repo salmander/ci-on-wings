189b0cab9d66472a2bbd6d10f2af9b352d9e07c6
Salman Ahmed <sahmed@andrewpage.com>
2015-07-30 17:07:17 +0100

    Added bootstrap

---
+++ application/views/template/template.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once 'header.php' ?>
+    </head>
+    <body>
+        <?php require_once 'nav_bar.php' ?>
+
+        <!-- Begin page content -->
+        <div class="container">
+          <div class="page-header">
+            <h1><?= ($heading) ?: 'Did you forget to add "$heading" missing?' ?></h1>
+          </div>
+          <?= ($main_content) ?: 'Missing body???' ?>
+        </div>
+
+        <footer class="footer">
+            <?php require_once 'footer.php' ?>
+        </footer>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="/assets/bootstrap/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="/assets/bootstrap/bootstrap-3.3.5-dist/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+</html>
